1fcb78fde07ed7268eea1fbdfed500c933dbdca8
BIEH01 <isaacescamilla.h1998@gmail.com>
2025-02-03 21:32:21 -0600

    Fixing images and fonts

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,10 +1,10 @@
-import addToCart from "/assets/images/icon-add-to-cart.svg";
-import carbonNeutral from "/assets/images/icon-carbon-neutral.svg";
-import decrementQuantity from "/assets/images/icon-decrement-quantity.svg";
-import incrementQuantity from "/assets/images/icon-increment-quantity.svg";
-import removeItem from "/assets/images/icon-remove-item.svg";
-import orderConfirmed from "/assets/images/icon-order-confirmed.svg";
-import illustrationEmptyCart from "/assets/images/illustration-empty-cart.svg";
+import addToCart from "/images/icon-add-to-cart.svg";
+import carbonNeutral from "/images/icon-carbon-neutral.svg";
+import decrementQuantity from "/images/icon-decrement-quantity.svg";
+import incrementQuantity from "/images/icon-increment-quantity.svg";
+import removeItem from "/images/icon-remove-item.svg";
+import orderConfirmed from "/images/icon-order-confirmed.svg";
+import illustrationEmptyCart from "/images/illustration-empty-cart.svg";
 
 export default {
 	addToCart,
