c4cea32c89626b624d27e27c14df4d09c01e1932
BIEH01 <isaacescamilla.h1998@gmail.com>
2025-02-03 21:03:44 -0600

    Fixing for deployment

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,10 +1,10 @@
-import addToCart from "#{$base-url}assets/images/icon-add-to-cart.svg";
-import carbonNeutral from "#{$base-url}assets/images/icon-carbon-neutral.svg";
-import decrementQuantity from "#{$base-url}assets/images/icon-decrement-quantity.svg";
-import incrementQuantity from "#{$base-url}assets/images/icon-increment-quantity.svg";
-import removeItem from "#{$base-url}assets/images/icon-remove-item.svg";
-import orderConfirmed from "#{$base-url}assets/images/icon-order-confirmed.svg";
-import illustrationEmptyCart from "#{$base-url}assets/images/illustration-empty-cart.svg";
+import addToCart from "../assets/images/icon-add-to-cart.svg";
+import carbonNeutral from "../assets/images/icon-carbon-neutral.svg";
+import decrementQuantity from "../assets/images/icon-decrement-quantity.svg";
+import incrementQuantity from "../assets/images/icon-increment-quantity.svg";
+import removeItem from "../assets/images/icon-remove-item.svg";
+import orderConfirmed from "../assets/images/icon-order-confirmed.svg";
+import illustrationEmptyCart from "../assets/images/illustration-empty-cart.svg";
 
 export default {
 	addToCart,
